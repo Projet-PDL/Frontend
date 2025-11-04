22f15dc38db0e47d41907647d33704ff2998dada
LanNg03 <lan.nguyen@etudiant.univ-rennes.fr>
2025-11-04 20:31:28 +0100

    Footer version 1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,202 @@
 <script setup lang="ts">
-
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
+  <div class="footer">
+    <h2>Get access to exclusive updates</h2>
 
+    <div class="newsletter-section">
+        <input type="email" class="form-control input-url" placeholder="your@email.com"/>
+        <button type="button" class="btn-newsletter">subscribe to the newsletter</button>
+    </div>
+
+    <div class="information-section">
+        <h1>LinkedIn2CV</h1>
+        <p class="information-text">Transform your LinkedIn profile into a professional CV in minutes. Join 15,000+ professionals who trust us with their career success.</p>
+        <ul class="footer-links">
+            <li>
+                <RouterLink to="/" class="link">Home</RouterLink>
+            </li>
+
+            <li>
+                <RouterLink to="/about" class="link">About</RouterLink>
+            </li>
+
+            <li>
+                <RouterLink to="/home" class="link">How it works</RouterLink>
+            </li>
+
+            <li>
+                <RouterLink to="/contact" class="link">Contact</RouterLink>
+            </li>
+        </ul>
+        <div class="social">
+          <i class="bi bi-facebook"></i>
+          <i class="bi bi-twitter"></i>
+          <i class="bi bi-instagram"></i>
+          <i class="bi bi-linkedin"></i>
+        </div>
+        <div class="footer-line"></div>
+        <div class="footer-bottom">
+          <p>© 2023 LinkedIn2CV. All rights reserved.</p>
+          <div class="left-part">
+            <p>Terms and conditions</p>
+            <p>Privacy policy</p>
+          </div>
+        </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.footer{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
 
+h2 {
+  color: #0F62A4;
+  font-size: 30px;
+  font-weight: 600;
+  margin: 70px 0 30px;
+}
+
+.newsletter-section {
+  position: relative;
+  width: 900px;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  height: 95px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.39);
+}
+
+.input-url {
+  width: 100%;
+  height: 100%; 
+  padding: 0 25px 0 25px;  
+  border: none;
+}
+
+.input-url::placeholder {
+  color: #99a0a6ff;
+  opacity: 1;
+}
+
+.input-url:focus {
+  box-shadow: none;
+}
+
+.btn-newsletter {
+  background-color: #0F62A4;
+  color: white;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+  border: none;
+  border-radius: 8px;
+  padding: 15px 40px;
+  text-decoration: none;
+  display: inline-block;
+  transition: background-color 0.3s ease, transform 0.3s ease;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+  position: absolute;
+  right: 25px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.btn-newsletter:hover {
+  background-color: rgb(24, 87, 151);
+}
+
+.information-section {
+  margin-top: -42px;
+  background-color: #0F62A4;
+  width : 100%;
+}
+
+.information-section h1 {
+  color: white;
+  font-size: 35px;
+  font-weight: 600;
+  margin: 100px 0 0;
+}
+
+.information-text{
+  color: white;
+  width: 45%;
+  margin: 30px auto;
+  font-size: 18px;
+}
+
+.footer-links {
+  display: flex;
+  justify-content: center;  
+  align-items: center;
+  gap: 30px;
+  list-style: none;
+  margin: 30px auto;
+}
+
+.link {
+  text-decoration: none; 
+  color: white;
+  font-size: 18px;
+  transition: color 0.3s ease;
+  font-weight: 600;
+}
+
+.link:hover {
+  transform: scale(1.05);
+  text-shadow: 0 1px 5px rgba(124, 174, 223, 0.35);
+}
+
+.social {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  margin: 30px auto;
+  color: white;
+  font-size: 20px;
+}
+
+.social i { 
+  cursor: pointer; 
+}
+
+.social i:hover {
+  transform: scale(1.05);
+}
+
+.footer-line {
+  width: 100%;
+  height: 1px;
+  background-color: white;
+  margin: 25px 0;
+}
+
+.footer-bottom {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 100px;
+  font-size: 14px;
+  color: white;
+  margin-bottom: 25px;
+}
+
+.footer-bottom p {
+  margin: 0;
+}
+
+.left-part {
+  display: flex;
+  gap: 60px;
+  color: white;
+}
 </style>
