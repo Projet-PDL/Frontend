d30056c8df19c8ab26d2849c98b32daf2b0b133f
manelgoudjil26 <manelgoudjil2@gmail.com>
2025-11-11 21:00:31 +0100

    Landing page v1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import NavbarHome from '../components/NavbarHome.vue'
+
 </script>
 
 <template>
   <main>
+    <NavbarHome />
     <TheWelcome />
   </main>
 </template>
